c02344b512c9f26e23b01fd90f3188f091f36e32
Kumari Anamika <75468908+IAFO-KumariAnamika@users.noreply.github.com>
2021-06-01 17:27:06 +0530

    Update form.js

--- js/form.js
+++ js/form.js
@@ -1,5 +1,33 @@
 class Form{
     constructor(){}
+    
+    display(){
+        var title=createElement('h2');
+        title.html("Car racing Game");
+        title.position(130,0);
+        
+        var input=createInput("name");
+        input.position(130,160);
+        
+        var button=createButton('PLAY');
+        button.position(250,200);
+        
+        var greetings=createElement('h3');
+        
+        button.mousePressed(function(){
+        input.hide();
+        button.hide();
+        
+        var name=input.value();
+        playerCount+=1;
+        player.update(name);
+        player.updateCount(playerCount);
+        
+        greetings.html("hello "+name);
+        greetings.position(130,160);
+        
+        })
+    }
 
     
 }
